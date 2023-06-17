4002a4b1709c36c94b2f0c1b5079058ffb18fe49
Luis Salas <lhoz@bu.edu>
2023-06-17 19:13:14 -0400

    Removed fixed header due complexities on minimal resolution glitches

--- js/component/app-header.js
+++ js/component/app-header.js
@@ -6,7 +6,7 @@ import * as Vue from "../lib/vue.js";
 export const AppHeader = Vue.defineCustomElement({
     // language=HTML
     template: `
-        <header class="container" ref="header">
+        <header class="container">
             <div class="container stretch">
                 <div class="row">
                     <div class="column">
@@ -31,39 +31,5 @@ export const AppHeader = Vue.defineCustomElement({
     styles: [`
         @import "css/common/default.css";
         @import "css/component/app-header.css";
-    `],
-
-    /* Component methods */
-    methods: {
-        /**
-         * Fixes the header to the top
-         */
-        fix: function () {
-            const header = this.$refs.header;
-            header.classList.add("fixed");
-        },
-
-        /**
-         * Removes the fixation and lets it act normally
-         */
-        unfix: function () {
-            const header = this.$refs.header;
-            header.classList.remove("fixed");
-        }
-    },
-
-    /* On ready */
-    mounted() {
-        const header = this.$refs.header;
-        const headerHeight = header.offsetHeight;
-        window.addEventListener("scroll", () => {
-            if ((window.innerWidth > 800)) {
-                if ((window.pageYOffset + headerHeight) > headerHeight) {
-                    this.fix();
-                } else {
-                    this.unfix();
-                }
-            }
-        });
-    }
+    `]
 });
