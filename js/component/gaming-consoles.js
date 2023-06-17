cd81e8e2d59e831e6238867bed91773cbec51cb9
Luis Salas <lhoz@bu.edu>
2023-06-17 14:42:44 -0400

    Added videogames section

---
+++ js/component/gaming-consoles.js
@@ -0,0 +1,60 @@
+import * as Vue from "../lib/vue.js";
+
+/**
+ * Displays the list of gaming consoles
+ */
+export const GamingConsoles = Vue.defineCustomElement({
+    // language=HTML
+    template: `
+        <div class="align-center">
+            <details v-for="console in consoles">
+                <summary>{{console.title}}</summary>
+                <div class="row">
+                    <div class="column">
+                        <figure>
+                            <img :src="console.img">
+                            <figcaption>
+                                {{console.caption}}
+                                <cite>{{console.cite}}</cite>
+                            </figcaption>
+                        </figure>
+                        <p>{{console.description}}</p>
+                        <ul class="list-plain">
+                            <li v-for="game in console.games">{{game}}</li>
+                        </ul>
+                    </div>
+                </div>
+            </details>
+        </div>
+    `,
+
+    // language=CSS
+    styles: [`
+        @import "css/common/default.css";
+        @import "css/component/gaming-consoles.css";
+    `],
+
+    /* Component properties */
+    props: {
+        src: {
+            type: String,
+            default: ""
+        }
+    },
+
+    /** Component data */
+    data() {
+        return {
+            consoles: []
+        }
+    },
+
+    /* Before loaded */
+    beforeMount() {
+        fetch(this.src, {
+            method: "get",
+            accept: "application/json"
+        }).then((response) => response.json()) //
+            .then((json) => this.consoles = json);
+    }
+});
---
+++ js/page/videogames.js
@@ -0,0 +1,13 @@
+import {ParticlesAnimation} from "../component/particles-animation.js";
+import {AppHeader} from "../component/app-header.js";
+import {AppFooter} from "../component/app-footer.js";
+import {NavBar} from "../component/nav-bar.js";
+import {GlitchText} from "../component/glitch-text.js";
+import {GamingConsoles} from "../component/gaming-consoles.js";
+
+customElements.define("particles-animation", ParticlesAnimation);
+customElements.define("app-header", AppHeader);
+customElements.define("app-footer", AppFooter);
+customElements.define("nav-bar", NavBar);
+customElements.define("glitch-text", GlitchText);
+customElements.define("gaming-consoles", GamingConsoles);
